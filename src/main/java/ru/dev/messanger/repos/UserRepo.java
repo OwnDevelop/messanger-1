4e218463293d6aac247cf3491daaa936d4f812bd
Иван <ivanmorenkov@mail.ru>
2018-06-02 00:17:23 +0400

    слияние начато

---
+++ src/main/java/ru/dev/messanger/repos/UserRepo.java
@@ -0,0 +1,8 @@
+package ru.dev.messanger.repos;
+
+import ru.dev.messanger.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepo extends JpaRepository<User, Long> {
+    User findByUsername(String username);
+}
