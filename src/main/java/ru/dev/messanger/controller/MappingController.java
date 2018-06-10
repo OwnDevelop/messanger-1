533dbb60c7176bb2f769fe4e5e6d99ea5e8100ff
Иван <ivanmorenkov@mail.ru>
2018-06-10 20:45:48 +0400

    tuk

--- src/main/java/ru/dev/messanger/controller/MappingController.java
+++ src/main/java/ru/dev/messanger/controller/MappingController.java
@@ -29,13 +29,13 @@ public class MappingController {
 
     @PostMapping("/enter")
     public String enter(@RequestParam String token) {
-        return bll.checkToken(token) ? "main" : "EnterPage ";
+        return bll.checkToken(token) ? "redirect:/main" : "redirect:/signin ";
     }
 
-//    @GetMapping("/main")
-//    public String main() {
-//        return "main";
-//    }
+    @GetMapping("/main")
+    public String main() {
+        return "main";
+    }
 
     @GetMapping("/signup")
     public String signup() {
